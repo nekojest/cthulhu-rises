95022cfde81133cbc55b63a061743e3f7b3c47cd
Sorin <sorinnechiforora@gmail.com>
2024-10-14 16:30:28 +0100

    Fix animation looping for all anims

--- Cthulhu.gd
+++ Cthulhu.gd
@@ -9,10 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	$CollisionShape2D.disabled = false	
-	$CthulhuSprite.play(&"idle")
-
-#func _on_attacked():
-	#print("testing")
+	$CthulhuSprite.play("idle")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -24,9 +21,9 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 		
 	# Play short attack animation and enable hitbox	
-	#if Input.is_action_just_pressed("short_atk")  and is_on_floor():
-		#$CthulhuSprite.play("short_atk")
-		#emit_signal("attacked")
+	if Input.is_action_just_pressed("short_atk")  and is_on_floor():
+		$CthulhuSprite.play("short_atk")
+		
 
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
@@ -38,6 +35,11 @@ func _physics_process(delta):
 			$CthulhuSprite.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$CthulhuSprite.play(&"idle")
+		#$CthulhuSprite.play(&"idle")
 
 	move_and_slide()
+
+
+func _on_cthulhu_sprite_animation_finished() -> void:
+	print("triggers?")
+	$CthulhuSprite.play("idle")
