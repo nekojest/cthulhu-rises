95ffc3718af60e095888bc1d1bcc2a08a93f62fa
Sorin <sorinnechiforora@gmail.com>
2024-10-14 16:03:56 +0100

    Import short attack sprite frames

--- Cthulhu.gd
+++ Cthulhu.gd
@@ -7,11 +7,12 @@ const JUMP_VELOCITY = -450.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _ready():
 	$CollisionShape2D.disabled = false	
 	$CthulhuSprite.play(&"idle")
 
+#func _on_attacked():
+	#print("testing")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,9 +22,12 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	# Play short attack animation and enable hitbox	
+	#if Input.is_action_just_pressed("short_atk")  and is_on_floor():
+		#$CthulhuSprite.play("short_atk")
+		#emit_signal("attacked")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		$CthulhuSprite.play(&"walk")
